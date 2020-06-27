139105eb140b064ea5edf989202da6a1931eeb2d
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-06-27 14:50:01 +0000

    Python Explained

---
+++ main.js
@@ -0,0 +1,82 @@
+/*
+
+boolean true or false
+
+string "" or ''
+
+number -1 1 1.0
+
+undefined
+null
+
+
+array = []
+
+object
+person = {
+    name: "Luis"
+}
+
+class 
+
+class Prueba {
+    name = "Luis"
+}
+
+p = Prueba();
+p.name
+
+person.name
+*/
+
+//typeof(a)
+
+// && || !
+
+let a = 1;
+const b = 2;
+var c = 3;
+
+// ternario
+let a = b > c ? 4 : 5;
+
+if(a > b){
+
+}else{
+
+}
+
+if (a > b && a > c){
+
+}else if (b > c){
+
+}else {
+
+}
+
+for(let i = 0; i<10; i++){
+
+}
+
+for(let x in valores){
+// indice
+}
+
+for(let x of valores){
+// elemento
+}
+
+let i = 0;
+while( i < 10){
+
+}
+
+
+function prueba(a, b=5){
+
+}
+
+const prueba = function(a, b=5){
+    return a + b
+}
+const prueba = (a, b) => a + b;
---
+++ main.py
@@ -0,0 +1,77 @@
+'''
+
+boolan True or False
+
+string "" or ''
+
+number -1 1 1.0
+
+None
+
+
+list ["one", "two", "3"]
+
+tuples ("one", "two", "3")
+
+set {"one", "two", "3"}
+
+dict {
+    "name": "Luis"
+}
+
+class
+
+class Prueba:
+
+
+'''
+#type(variable)
+
+# and or not
+
+a = 1
+b = 2
+c = 3
+
+# ternario 
+a = 4 if b > c else 5
+
+if a > b :
+    pass
+else:
+    pass
+
+if a > b and a > c:
+    pass
+elif b > c:
+    pass
+else:
+    pass
+
+
+# range(start, stop, step) start=0, step=1
+for x in range(1, 11, 2):
+    print(x) # 1, 3, 5, 7, 9
+
+valores = ["one", "two", "3"]
+for x in range(len(valores)): # 3 2
+    print(valores[x]) # one, two, 3 
+
+
+valores = ["one", "two", "3"] 
+for x in valores:
+    print(x) # one, two, 3
+
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+
+def prueba(a, b=5):
+    pass
+
+
+prueba = lambda a=5, b=5: a + b
+
+prueba()
